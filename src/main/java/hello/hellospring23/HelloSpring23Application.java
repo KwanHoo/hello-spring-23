b5757004c38eb76d381d905fb63dc16f0442d5c6
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-12-08 14:24:03 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring23/HelloSpring23Application.java
@@ -0,0 +1,13 @@
+package hello.hellospring23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpring23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpring23Application.class, args);
+	}
+
+}
---
+++ src/test/java/hello/hellospring23/HelloSpring23ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospring23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
