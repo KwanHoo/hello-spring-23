792cc42e53e9a1bc25fd4572b2b023258236c169
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-12-10 19:18:44 +0900

    섹션5-3_회원 웹 기능 - 조회

--- src/main/java/hello/hellospring23/controller/MemberController.java
+++ src/main/java/hello/hellospring23/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring23.domain.Member;
 import hello.hellospring23.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -28,6 +31,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";    // 끝나면 홈화면으로 돌려보내는거
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
