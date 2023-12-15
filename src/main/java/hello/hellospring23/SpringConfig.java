cc1630c86587872ceb7b154248b0f25cf5c3baf3
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-12-15 22:28:51 +0900

    섹션6-2_순수 JDBC

--- src/main/java/hello/hellospring23/SpringConfig.java
+++ src/main/java/hello/hellospring23/SpringConfig.java
@@ -1,15 +1,27 @@
 package hello.hellospring23;
 
+import hello.hellospring23.repository.JdbcMemberRepository;
 import hello.hellospring23.repository.MemberRepository;
 import hello.hellospring23.repository.MemoryMemberRepository;
 import hello.hellospring23.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 
 // 자바코드로 직접 스프링 빈 등록
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
 //        return new MemberService(memberRepository());    //에러뜸
@@ -18,6 +30,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);    // 에러뜸
     }
 }
