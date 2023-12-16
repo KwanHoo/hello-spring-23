817b46a648fa3e22a9d8b9a7e7b80e7cbd6d6bab
KwanHoo <49335804+KwanHoo@users.noreply.github.com>
2023-12-16 15:19:05 +0900

    섹션6-5_JPA

--- src/main/java/hello/hellospring23/SpringConfig.java
+++ src/main/java/hello/hellospring23/SpringConfig.java
@@ -1,10 +1,8 @@
 package hello.hellospring23;
 
-import hello.hellospring23.repository.JdbcMemberRepository;
-import hello.hellospring23.repository.JdbcTemplateMemberRepository;
-import hello.hellospring23.repository.MemberRepository;
-import hello.hellospring23.repository.MemoryMemberRepository;
+import hello.hellospring23.repository.*;
 import hello.hellospring23.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +14,17 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource){
+//        this.dataSource = dataSource;
+//    }
 
+    private EntityManager em;
     @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em){
+        this.em = em;
     }
 
     @Bean
@@ -33,6 +37,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);    // 에러뜸
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
